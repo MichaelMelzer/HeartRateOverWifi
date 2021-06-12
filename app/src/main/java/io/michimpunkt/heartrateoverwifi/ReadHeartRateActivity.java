ff8687162a2bbd9ccff8fc68795354027f539497
Michael Melzer <michael.melzer@stud.h-da.de>
2021-06-12 09:02:52 +0200

    #4 Websocket should now work (UNTESTED)

--- app/src/main/java/io/michimpunkt/heartrateoverwifi/ReadHeartRateActivity.java
+++ app/src/main/java/io/michimpunkt/heartrateoverwifi/ReadHeartRateActivity.java
@@ -14,12 +14,14 @@ import android.widget.Toast;
 
 import java.io.IOException;
 
+import fi.iki.elonen.NanoWSD;
 import io.michimpunkt.heartrateoverwifi.databinding.ActivityReadHeartRateBinding;
 
 public class ReadHeartRateActivity extends Activity implements SensorEventListener {
 
     private TextView mTextView;
     private ActivityReadHeartRateBinding binding;
+    private WebSocketServer webSocketServer;
     private HttpServer httpServer;
 
     @Override
@@ -29,10 +31,16 @@ public class ReadHeartRateActivity extends Activity implements SensorEventListen
         binding = ActivityReadHeartRateBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-        sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        // set up websocket first
+        try {
+            webSocketServer = new WebSocketServer(getLocalIp(), 8081);
+        } catch (IOException e) {
+            Log.e(getClass().getSimpleName(), "Could not start webSocketServer", e);
+            Toast.makeText(this, "Could not start Websocket server", Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
+        // after that set up http server
         try {
             httpServer = new HttpServer(this, getLocalIp(), 8080);
         } catch (IOException e) {
@@ -40,6 +48,12 @@ public class ReadHeartRateActivity extends Activity implements SensorEventListen
             Toast.makeText(this, "Could not start HTTP server", Toast.LENGTH_SHORT).show();
             finish();
         }
+
+        // only after we have that, we wanna measure the heart rate
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
         Log.i(getClass().getSimpleName(), "onCreate() finished");
     }
 
@@ -52,8 +66,19 @@ public class ReadHeartRateActivity extends Activity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
             float heartRate = event.values[0];
+
             TextView tvHeartRate = findViewById(R.id.tvHeartRate);
             tvHeartRate.setText(String.valueOf(Math.round(heartRate)) + " BPM");
+
+            for (NanoWSD.WebSocket socket : webSocketServer.getConnections()) {
+                try {
+                    socket.send(String.valueOf(Math.round(heartRate)));
+                } catch (IOException e) {
+                    // TODO add user feedback
+                    Log.e(getClass().getSimpleName(), "Could not send status", e);
+                }
+            }
+
             Log.i(getClass().getSimpleName(), event.values + " BPM - " + event.accuracy + " Accuracy @"+event.timestamp);
         }
     }
